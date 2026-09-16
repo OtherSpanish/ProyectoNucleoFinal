969eae7b0708a31a30a79a816f63c31c5ae28a50
Juan Carlos <juankamome95@gmail.com>
2026-09-16 08:24:46 -0500

    Uploaded front & back empty files

---
+++ aulasync/src/test/java/unbosque/edu/co/aulasync/AulasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package unbosque.edu.co.aulasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
